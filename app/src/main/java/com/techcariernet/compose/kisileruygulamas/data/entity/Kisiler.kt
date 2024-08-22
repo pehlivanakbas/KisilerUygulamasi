173bdc88019203850e4b5a055b905a7bf05a35c9
PehlivanAkbas <pehlivanakbas@gmail.com>
2024-08-22 19:22:07 +0300

    Kisiler uygulamasi

---
+++ app/src/main/java/com/techcariernet/compose/kisileruygulamas/data/entity/Kisiler.kt
@@ -0,0 +1,5 @@
+package com.techcariernet.compose.kisileruygulamas.data.entity
+
+data class Kisiler(var kisi_id: Int,
+                   var kisi_ad: String,
+                   var kisi_tel: String) {}
